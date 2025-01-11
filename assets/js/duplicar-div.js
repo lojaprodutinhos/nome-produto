17941b5a1331676b581ab420027b0b2b5e411ec8
lojaprodutinhos <gabriel.a.d.esperidiao@gmail.com>
2025-01-11 01:49:30 -0300

    update

---
+++ assets/js/duplicar-div.js
@@ -0,0 +1,34 @@
+async function duplicateDiv(duplicateTag) {
+  await new Promise(resolve => setTimeout(resolve, 100)); // 100ms de atraso
+  const originalDiv =  document.getElementById(duplicateTag);
+
+  if (!originalDiv) {
+    
+    console.error(`Div com o ID "${duplicateTag}" não encontrada.`);
+      return;
+  }
+
+  const parent = originalDiv.parentNode;
+
+  for (let i = 1; i <= 3; i++) {
+      // Cria uma nova div
+      const newDiv = document.createElement('div');
+
+      //Copia a classe
+      newDiv.className = originalDiv.className;
+
+      // Copia os estilos da original
+      newDiv.style.cssText = originalDiv.style.cssText;
+
+      // Copia o conteúdo textual da original
+      newDiv.innerHTML = originalDiv.innerHTML;
+
+      // Define um novo ID para a nova div
+      newDiv.id = `${duplicateTag}-clone-${i}`;
+
+      // Adiciona a nova div ao pai da original
+      parent.appendChild(newDiv);
+  }
+}
+
+duplicateDiv("cartaoCurso");
---
+++ assets/js/tag-link.js
@@ -0,0 +1,16 @@
+// Função para carregar conteúdo de outro arquivo
+async function loadHTML(selector, file) {
+const element = document.querySelector(selector);
+const response = await fetch(file);
+    if (response.ok) {
+        element.innerHTML = await response.text();
+    } else {
+        console.error(`Erro ao carregar ${file}: ${response.statusText}`);
+    }
+}
+
+loadHTML("#header-site", "sections/header.html");
+loadHTML("#footer-site", "sections/footer.html");
+loadHTML("#primeira-sect", "sections/primeira-section-index.html");
+loadHTML("#segunda-sect", "sections/segunda-section-index.html");
+loadHTML("#terceira-sect", "sections/terceira-section-index.html");
