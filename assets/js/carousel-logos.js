21f10dd2dd354830dcd1f8ead2e8106de7625ab1
lojaprodutinhos <gabriel.a.d.esperidiao@gmail.com>
2025-01-13 18:46:41 -0300

    update

--- assets/js/carousel-logos.js
+++ assets/js/carousel-logos.js
@@ -1,57 +1,36 @@
-const items = Array.from(Array(12).keys());
-// Função para agrupar os itens em slides
-function createCarouselItems(items, itemsPerSlide) {
-    const carouselContent = document.getElementById("carouselContent");
-    carouselContent.innerHTML = ""; // Limpa o conteúdo anterior
-
-    let currentSlide;
-    items.forEach((item, index) => {
-        if (index % itemsPerSlide === 0) {
-            // Cria um novo slide
-            currentSlide = document.createElement("div");
-            currentSlide.classList.add("carousel-item");
-            if (index === 0) currentSlide.classList.add("active", "px-5"); // Primeiro slide ativo
-            carouselContent.appendChild(currentSlide);
+function procurandoCarousel(carouselContainer) {
+    let lastScrollLeft = 0;
+    const observer = new MutationObserver(() => {
+        const scrollContainer = document.getElementById(carouselContainer);
+        if (scrollContainer) {
+            observer.disconnect(); // Para de observar quando o elemento for encontrado
         }
+        scrollContainer.addEventListener('scroll', () => {
+            const currentScrollLeft = scrollContainer.scrollLeft;
+            const angleLeft = document.getElementById("angle-left");
+            const angleRight = document.getElementById("angle-right");
 
-        const itemDiv = document.createElement("div");
-        itemDiv.setAttribute("class","col");
 
-        // Cria o elemento do item
-        const itemDivImg = document.createElement("img");
-        itemDivImg.setAttribute("src",`/nome-produto/imagens/primeira_sect/logo_${item}.png`);
-        itemDivImg.setAttribute("style", "width: 11vw; height: 10vh;");
-        itemDivImg.setAttribute("alt", "package");
-        itemDivImg.setAttribute("id","logo-LP");
+            if (currentScrollLeft > lastScrollLeft) {
+                console.log('Scrolling to the right');
+                angleLeft.setAttribute("style", "background: rgba(0,0,0,0.5);");
+            } else if (currentScrollLeft < lastScrollLeft) {
+                console.log('Scrolling to the left');
+                angleRight.setAttribute("style", "background: rgba(0,0,0,0.5);");
+            }
+        
+            lastScrollLeft = currentScrollLeft; // Atualiza a posição anterior
 
-        // Adiciona o item ao slide atual
-        itemDiv.appendChild(itemDivImg);
-        currentSlide.appendChild(itemDiv);
+            clearTimeout(lastScrollLeft);
+            lastScrollLeft = setTimeout(() => {
+                console.log('Scroll horizontal parado');
+                angleLeft.setAttribute("style", "background: none;");
+                angleRight.setAttribute("style", "background: none;");
+            }, 200);
+            });
     });
+    // Configura o observer para monitorar alterações no DOM
+    observer.observe(document.body, { childList: true, subtree: true });
 }
 
-// Função para atualizar o carousel com base na largura da tela
-function updateCarousel() {
-    const screenWidth = window.innerWidth;
-    let itemsPerSlide;
-
-    if (screenWidth <= 992 && screenWidth >=720) {
-        itemsPerSlide = 6; // Telas grandes
-    } else if (screenWidth <= 719 && screenWidth >= 572) {
-        itemsPerSlide = 5; // Telas médias
-    } else if (screenWidth <= 571 && screenWidth >= 472) {
-        itemsPerSlide = 4; // Telas pequenas
-    } else if (screenWidth <= 471 && screenWidth >= 372) {
-        itemsPerSlide = 3; // Telas pequenas
-    } else if (screenWidth <= 371 && screenWidth >= 272) {
-        itemsPerSlide = 2; // Telas pequenas
-    } else {
-        itemsPerSlide = 1; // Telas extra pequenas
-    }
-
-    createCarouselItems(items, itemsPerSlide);
-}
-
-// Atualiza o carousel ao carregar a página e ao redimensionar a janela
-window.addEventListener("load", updateCarousel);
-window.addEventListener("resize", updateCarousel);
+procurandoCarousel("scrollContainer");
