0fa92e8e0c1ae5d58d93d4062fc51b66419cd84b
lojaprodutinhos <gabriel.a.d.esperidiao@gmail.com>
2025-01-14 21:28:24 -0300

    update

--- assets/js/carousel-logos.js
+++ assets/js/carousel-logos.js
@@ -14,22 +14,22 @@ function procurandoCarousel(carouselContainer) {
             const currentScrollLeft = scrollContainer.scrollLeft;
             if (currentScrollLeft > lastScrollLeft) {
                 // Scrolling to the right
-                rightIndicator.style.backgroundColor = 'rgba(0,0,0,0.4)';
-                leftIndicator.style.backgroundColor = 'transparent';
-                rightIndicator.style.color = 'rgba(255,255,255,1)';
+                // rightIndicator.style.backgroundColor = 'rgba(0,0,0,0.4)';
+                // leftIndicator.style.backgroundColor = 'transparent';
+                rightIndicator.style.color = 'rgba(0,0,0,0.1)';
                 leftIndicator.style.color = 'transparent';
             } else if (currentScrollLeft < lastScrollLeft) {
                 // Scrolling to the left
-                leftIndicator.style.backgroundColor = 'rgba(0,0,0,0.4)';
-                rightIndicator.style.backgroundColor = 'transparent';
-                leftIndicator.style.color = 'rgba(255,255,255,1)';
+                // leftIndicator.style.backgroundColor = 'rgba(0,0,0,0.4)';
+                // rightIndicator.style.backgroundColor = 'transparent';
+                leftIndicator.style.color = 'rgba(0,0,0,0.1)';
                 rightIndicator.style.color = 'transparent';
             }
             lastScrollLeft = currentScrollLeft;
             // Reset the background colors when scrolling stops
             scrollTimeout = setTimeout(() => {
-                leftIndicator.style.backgroundColor = 'transparent';
-                rightIndicator.style.backgroundColor = 'transparent';
+                // leftIndicator.style.backgroundColor = 'transparent';
+                // rightIndicator.style.backgroundColor = 'transparent';
                 rightIndicator.style.color = 'transparent';
                 leftIndicator.style.color = 'transparent';
             }, 200); // Adjust the delay as needed
